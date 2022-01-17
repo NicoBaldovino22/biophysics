c336ee68b8f76efb436c25b7d1bfc1f975aa2329
NicoBaldovino22 <nico.baldovino@oit.edu>
2022-01-17 15:28:38 -0800

    Create computer-lab-1.py

---
+++ week-02/computer-lab-1.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "b7cf91a4",
+   "metadata": {},
+   "source": [
+    "# Computer Lab 1\n",
+    "\n",
+    "Work through the first half of \"First Computer Lab\" (Section 5.1) in _A Student's Guide to Python._  This exercise ties together everything we did in Week 1.\n",
+    "\n",
+    "You can use this notebook to store your code, plots, and notes, or you can write a ___script___ (see Section 3.3) that will carry out the analysis and produce the plots, and simply write your comments in this notebook.  If you write a script, it should save your plot with the data and your best fit to a PDF in this folder when it runs."
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = 3
+print('\n'+str(a))
